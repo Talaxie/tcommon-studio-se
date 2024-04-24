0c467052117ac63de8649738a56a2535bb31b886
JeanCazaux <jeancazaux@hotmail.com>
2024-04-24 09:11:13 +0200

    feat: Copyright

--- main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/model/context/link/ProcessItemContextLinkService.java
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/model/context/link/ProcessItemContextLinkService.java
@@ -8,7 +8,7 @@ package org.talend.core.model.context.link;
 // %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
 //
 // You should have received a copy of the agreement
-// along with this program; if not, write to Talaxie SA
+// along with this program; if not, write to Talend SA
 // 9 rue Pages 92150 Suresnes, France
 //
 // ============================================================================
--- main/plugins/org.talend.core.runtime/src/main/java/org/talend/repository/ReferenceProjectProblemManager.java
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/repository/ReferenceProjectProblemManager.java
@@ -7,7 +7,7 @@ package org.talend.repository;
 // %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
 //
 // You should have received a copy of the agreement
-// along with this program; if not, write to Talaxie SA
+// along with this program; if not, write to Talend SA
 // 9 rue Pages 92150 Suresnes, France
 //
 // ============================================================================
